d10093cbed52e8c1d618241c966fba4ff0b89f24
Zachariah Petsrillo <zpetsrillo@Zachariahs-MacBook-Pro-2.local>
2024-01-27 17:05:11 -0600

    Revert "Add tailwindcss"
    This reverts commit 3d9b5077144cb1750fa5cc547ffd220b6f3693c1.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 
-import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://zpetsrillo.com",
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap()],
 });
